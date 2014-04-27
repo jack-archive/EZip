71c3aad0fecb95a1e609cc6f46ca9c596bb84538
Jack Maloney <jmmaloney4@gmail.com>
2014-04-26 20:24:21 -0500

    Initial Commit

---
+++ ez/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ez
+//
+//  Created by Jack Maloney on 4/26/14.
+//  Copyright (c) 2014 Jack Maloney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
